a5b47ac37d979631ff7c52006c64681c0482def4
Mikhail at dev4traders <mikha.dev@gmail.com>
2020-12-16 13:26:36 +0100

    fix

--- config/default-settings.php
+++ config/default-settings.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'api_url' => 'http://accounts.dev4traders.com/api/',
+    'api_url' => 'http://copier.dev4traders.com/api/',
     'api_key' => '',
     'debug_log_level' => 'warning',
 );
